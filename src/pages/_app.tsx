ed4ec50cd477037f229082bcdadf2d51d86f0426
Marcelo Pecin <celopeccin@gmail.com>
2022-07-21 14:29:48 +0200

    feat: Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '@/theme'
@@ -6,11 +7,28 @@ import Layout from '@/components/layout'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="g-tag" strategy="lazyOnload">
+        {`
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+             page_path: window.location.pathname,
+             });
+         `}
+      </Script>
+
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
   )
 }
 
