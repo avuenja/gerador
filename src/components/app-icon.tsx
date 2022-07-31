22fcc00bfdc937cb1a9ccc11045c77f322f279be
Marcelo Pecin <celopeccin@gmail.com>
2022-08-01 00:08:12 +0200

    fix: build error, IconProps

--- src/components/app-icon.tsx
+++ src/components/app-icon.tsx
@@ -1,14 +1,14 @@
-import { Icon, type IconProps } from '@chakra-ui/react'
+import { Icon } from '@chakra-ui/react'
 import type { IconProps as PhosphorIconProps } from 'phosphor-react'
 
-interface AppIconProps extends IconProps {
+interface AppIconProps {
   icon: React.ForwardRefExoticComponent<
     PhosphorIconProps & React.RefAttributes<SVGSVGElement>
   >
 }
 
-const AppIcon = ({ icon, ...props }: AppIconProps) => {
-  return <Icon as={icon} width="6" height="6" {...props} />
+const AppIcon = ({ icon }: AppIconProps) => {
+  return <Icon as={icon} width="6" height="6" />
 }
 
 export default AppIcon
